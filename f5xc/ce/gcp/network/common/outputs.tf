c71e26d946c98e31b8598613368efb169302a82b
c.klewar <c.klewar@f5.com>
2024-04-18 01:24:24 +0200

    add gcp module v7

--- f5xc/ce/gcp/network/common/outputs.tf
+++ f5xc/ce/gcp/network/common/outputs.tf
@@ -3,6 +3,6 @@ output "common" {
     slo_network    = var.create_network ? google_compute_network.slo.0 : data.google_compute_network.slo.0
     sli_network    = var.create_network && var.is_multi_nic ? google_compute_network.sli.0 : var.create_network == false && var.is_multi_nic ? data.google_compute_network.sli.0 : null
     slo_subnetwork = var.create_subnetwork ? google_compute_subnetwork.slo.0 : data.google_compute_subnetwork.slo.0
-    sli_subnetwork = var.create_subnetwork && var.is_multi_nic ? google_compute_subnetwork.sli.0 : data.google_compute_subnetwork.sli.0
+    sli_subnetwork = var.create_subnetwork && var.is_multi_nic ? google_compute_subnetwork.sli.0 : var.is_multi_nic ? data.google_compute_subnetwork.sli.0 : null
   }
 }
