f5dae47b6a3c9c32ba1c4499ae8e8d1616584615
c.klewar <c.klewar@f5.com>
2024-04-19 14:16:15 +0200

    add f5xc_azure_cloud_ce_single_node_single_nic_new_vnet_new_subnet v11

--- f5xc/ce/azure/network/node/main.tf
+++ f5xc/ce/azure/network/node/main.tf
@@ -6,36 +6,6 @@ resource "azurerm_public_ip" "ip" {
   allocation_method   = var.azurerm_public_ip_allocation_method
 }
 
-resource "azurerm_network_interface" "slo" {
-  name                          = "${var.f5xc_node_name}-slo"
-  location                      = var.f5xc_azure_region
-  resource_group_name           = var.azurerm_resource_group_name
-  enable_ip_forwarding          = var.enable_ip_forwarding
-  enable_accelerated_networking = var.enable_accelerated_networking
-
-  ip_configuration {
-    name                          = "slo"
-    subnet_id                     = azurerm_subnet.slo.id
-    private_ip_address_allocation = var.azurerm_private_ip_address_allocation_method
-    public_ip_address_id          = var.has_public_ip ? azurerm_public_ip.ip[0].id : null
-  }
-}
-
-resource "azurerm_network_interface" "sli" {
-  count                         = var.is_multi_nic ? 1 : 0
-  name                          = "${var.f5xc_node_name}-sli"
-  location                      = var.f5xc_azure_region
-  resource_group_name           = var.azurerm_resource_group_name
-  enable_ip_forwarding          = var.enable_ip_forwarding
-  enable_accelerated_networking = var.enable_accelerated_networking
-
-  ip_configuration {
-    name                          = "sli"
-    subnet_id                     = azurerm_subnet.sli.*.id
-    private_ip_address_allocation = var.azurerm_private_ip_address_allocation_method
-  }
-}
-
 resource "azurerm_subnet" "slo" {
   name                 = format("%s-subnet-slo", var.f5xc_node_name)
   address_prefixes     = [var.azurerm_subnet_slo_address_prefix]
@@ -57,6 +27,36 @@ resource "azurerm_subnet" "sli" {
   }*/
 }
 
+resource "azurerm_network_interface" "slo" {
+  name                          = "${var.f5xc_node_name}-slo"
+  location                      = var.f5xc_azure_region
+  resource_group_name           = var.azurerm_resource_group_name
+  enable_ip_forwarding          = var.enable_ip_forwarding
+  enable_accelerated_networking = var.enable_accelerated_networking
+
+  ip_configuration {
+    name                          = "slo"
+    subnet_id                     = azurerm_subnet.slo.id
+    private_ip_address_allocation = var.azurerm_private_ip_address_allocation_method
+    public_ip_address_id          = var.has_public_ip ? azurerm_public_ip.ip.0.id : null
+  }
+}
+
+resource "azurerm_network_interface" "sli" {
+  count                         = var.is_multi_nic ? 1 : 0
+  name                          = "${var.f5xc_node_name}-sli"
+  location                      = var.f5xc_azure_region
+  resource_group_name           = var.azurerm_resource_group_name
+  enable_ip_forwarding          = var.enable_ip_forwarding
+  enable_accelerated_networking = var.enable_accelerated_networking
+
+  ip_configuration {
+    name                          = "sli"
+    subnet_id                     = azurerm_subnet.sli.0.id
+    private_ip_address_allocation = var.azurerm_private_ip_address_allocation_method
+  }
+}
+
 resource "azurerm_route_table" "sli" {
   count               = var.is_multi_nic ? 1 : 0
   name                = format("%s-sli-rt", var.f5xc_node_name)
@@ -67,14 +67,14 @@ resource "azurerm_route_table" "sli" {
     name                   = format("%s-sli-default-route", var.f5xc_node_name)
     address_prefix         = "0.0.0.0/0"
     next_hop_type          = var.azurerm_route_table_next_hop_type
-    next_hop_in_ip_address = cidrhost(azurerm_subnet.sli.*.address_prefixes, 1)
+    next_hop_in_ip_address = cidrhost(azurerm_subnet.sli.0.address_prefixes[0], 1)
   }
 }
 
 resource "azurerm_subnet_route_table_association" "sli" {
   count          = var.is_multi_nic ? 1 : 0
-  subnet_id      = azurerm_subnet.sli.*.id
-  route_table_id = azurerm_route_table.sli.*.id
+  subnet_id      = azurerm_subnet.sli.0.id
+  route_table_id = azurerm_route_table.sli.0.id
 }
 
 resource "azurerm_network_interface_security_group_association" "sga_slo" {
@@ -84,6 +84,6 @@ resource "azurerm_network_interface_security_group_association" "sga_slo" {
 
 resource "azurerm_network_interface_security_group_association" "sga_sli" {
   count                     = var.is_multi_nic ? 1 : 0
-  network_interface_id      = azurerm_network_interface.sli.*.id
+  network_interface_id      = azurerm_network_interface.sli.0.id
   network_security_group_id = var.azurerm_security_group_sli_id
 }
