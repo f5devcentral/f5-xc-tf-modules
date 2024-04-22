b5c1dea22601dade8db00c8f15d3f3af125a5f3b
Patrick Cable <p.cable@f5.com>
2024-04-22 13:57:50 -0700

    Tenant ID is not used

--- utils/maurice/locals.tf
+++ utils/maurice/locals.tf
@@ -1,8 +1,7 @@
 locals {
   base_offset               = length(substr(var.f5xc_api_url, 8, -1)) - 4
-  tenant_id                 = split(".", substr(var.f5xc_api_url, 8, -1))[0]
   base_domain_list          = split(".", split("/", substr(var.f5xc_api_url, 8, -1))[0])
   base_domain               = join(".", slice(local.base_domain_list, 1, length(local.base_domain_list)))
-  maurice_endpoint_url      = format("https://register.%s.%s", local.tenant_id, local.base_domain)
-  maurice_mtls_endpoint_url = format("https://register-tls.%s.%s", local.tenant_id, local.base_domain)
+  maurice_endpoint_url      = format("https://register.%s", local.base_domain)
+  maurice_mtls_endpoint_url = format("https://register-tls.%s", local.base_domain)
 }
