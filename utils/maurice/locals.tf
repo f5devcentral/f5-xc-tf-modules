e5bb34953b940f7cd6bce46aeab6c2a661a96c06
c.klewar <c.klewar@f5.com>
2024-04-23 23:57:42 +0200

    fix maurice production ep v1

--- utils/maurice/locals.tf
+++ utils/maurice/locals.tf
@@ -1,7 +1,8 @@
 locals {
   is_production_env         = endswith(var.f5xc_api_url, var.production_api_base)
+  tenant_id                 = split(".", substr(var.f5xc_api_url, 8, -1))[0]
   base_domain_list          = split(".", split("/", substr(var.f5xc_api_url, 8, -1))[0])
   base_domain               = join(".", slice(local.base_domain_list, 1, length(local.base_domain_list)))
-  maurice_endpoint_url      = local.is_production_env ? format("https://register.%s", local.base_domain) : format("https://register.%s", join(".", slice(local.base_domain_list, 2, length(local.base_domain_list))))
-  maurice_mtls_endpoint_url = local.is_production_env ? format("https://register-tls.%s", local.base_domain) : format("https://register-tls.%s", join(".", slice(local.base_domain_list, 2, length(local.base_domain_list))))
+  maurice_endpoint_url      = local.is_production_env ? format("https://register.%s.%s", local.tenant_id, join(".", slice(local.base_domain_list, 2, length(local.base_domain_list)))) : format("https://register.%s", local.base_domain)
+  maurice_mtls_endpoint_url = local.is_production_env ? format("https://register-tls.%s.%s", local.tenant_id, join(".", slice(local.base_domain_list, 2, length(local.base_domain_list)))) : format("https://register-tls.%s", local.base_domain)
 }
