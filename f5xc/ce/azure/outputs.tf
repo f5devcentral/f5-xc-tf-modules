84157fe0b6d0571aa05f334c301e3cc9be1659d4
c.klewar <c.klewar@f5.com>
2024-04-23 21:05:04 +0200

    fix maurice production ep

--- f5xc/ce/azure/outputs.tf
+++ f5xc/ce/azure/outputs.tf
@@ -14,5 +14,6 @@ output "ce" {
         }
       }
     }
+    maurice = module.maurice.endpoints
   }
 }
--- utils/maurice/locals.tf
+++ utils/maurice/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  base_offset               = length(substr(var.f5xc_api_url, 8, -1)) - 4
+  is_production_env         = endswith(var.f5xc_api_url, var.production_api_base)
   base_domain_list          = split(".", split("/", substr(var.f5xc_api_url, 8, -1))[0])
   base_domain               = join(".", slice(local.base_domain_list, 1, length(local.base_domain_list)))
-  maurice_endpoint_url      = format("https://register.%s", local.base_domain)
-  maurice_mtls_endpoint_url = format("https://register-tls.%s", local.base_domain)
+  maurice_endpoint_url      = local.is_production_env ? format("https://register.%s", local.base_domain) : format("https://register.%s", join(".", slice(local.base_domain_list, 2, length(local.base_domain_list))))
+  maurice_mtls_endpoint_url = local.is_production_env ? format("https://register-tls.%s", local.base_domain) : format("https://register-tls.%s", join(".", slice(local.base_domain_list, 2, length(local.base_domain_list))))
 }
