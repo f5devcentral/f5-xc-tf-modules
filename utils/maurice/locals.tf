c2112c85fd65ec89f48f7b5980e7ec89665459fe
c.klewar <c.klewar@f5.com>
2024-04-24 00:11:13 +0200

    fix maurice v4

--- utils/maurice/locals.tf
+++ utils/maurice/locals.tf
@@ -3,6 +3,6 @@ locals {
   tenant_id                 = split(".", substr(var.f5xc_api_url, 8, -1))[0]
   base_domain_list          = split(".", split("/", substr(var.f5xc_api_url, 8, -1))[0])
   base_domain               = join(".", slice(local.base_domain_list, 1, length(local.base_domain_list)))
-  maurice_endpoint_url      = local.is_production_env ? format("https://register.%s.%s", local.tenant_id, join(".", slice(local.base_domain_list, 2, length(local.base_domain_list)))) : format("https://register.%s", local.base_domain)
-  maurice_mtls_endpoint_url = local.is_production_env ? format("https://register-tls.%s.%s", local.tenant_id, join(".", slice(local.base_domain_list, 2, length(local.base_domain_list)))) : format("https://register-tls.%s", local.base_domain)
+  maurice_endpoint_url      = local.is_production_env ? format("https://register.%s", join(".", slice(local.base_domain_list, 2, length(local.base_domain_list)))) : format("https://register.%s.%s", local.tenant_id, local.base_domain)
+  maurice_mtls_endpoint_url = local.is_production_env ? format("https://register-tls.%s", join(".", slice(local.base_domain_list, 2, length(local.base_domain_list)))) : format("https://register-tls.%s.%s", local.tenant_id, local.base_domain)
 }
