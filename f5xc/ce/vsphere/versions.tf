2ddf96c909edf4a1e62b144ad264366c62d3ce3d
c.klewar <c.klewar@f5.com>
2024-05-24 15:04:15 +0200

    v0.11.32

--- f5xc/ce/vsphere/versions.tf
+++ f5xc/ce/vsphere/versions.tf
@@ -1,18 +1,17 @@
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = ">= 2.3.1"
+    }
+
     volterra = {
       source  = "volterraedge/volterra"
       version = "= 0.11.32"
     }
 
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.48.0"
-    }
-
     local = ">= 2.2.3"
-    null = ">= 3.1.1"
+    null  = ">= 3.1.1"
+
   }
 }
