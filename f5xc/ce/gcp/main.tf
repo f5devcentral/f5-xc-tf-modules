1ad6431ae73d22ad515298fa6b62e55b28072d09
c.klewar <c.klewar@f5.com>
2024-10-24 14:57:10 +0200

    add rseries support v7

--- f5xc/ce/gcp/main.tf
+++ f5xc/ce/gcp/main.tf
@@ -53,6 +53,8 @@ module "firewall" {
 module "secure_mesh_site_v2" {
   count                                      = var.f5xc_secure_mesh_site_version == 2 && var.f5xc_sms_provider_name != null ? 1 : 0
   source                                     = "../../secure_mesh_site_v2"
+  f5xc_api_url                               = var.f5xc_api_url
+  f5xc_api_token                             = var.f5xc_api_token
   f5xc_tenant                                = var.f5xc_tenant
   f5xc_sms_name                              = var.f5xc_cluster_name
   f5xc_namespace                             = var.f5xc_namespace
